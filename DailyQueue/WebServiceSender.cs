e672f551f3a81c656eb465d87c4a9bb4f0b81434
Calvin Wiebe <calvin.wiebe@gmail.com>
2011-08-06 14:33:04 -0400

    code cleanup in websender

--- DailyQueue/WebServiceSender.cs
+++ DailyQueue/WebServiceSender.cs
@@ -20,15 +20,10 @@ namespace DailyQueue
             }
 
             bool trying = true;
+            CookieContainer cookieJar = new CookieContainer();
 
             while (trying)
             {
-
-                // used to build entire input
-                StringBuilder sb = new StringBuilder();
-
-                // used on each read operation
-                byte[] buf = new byte[8192];
 
                 //the request
                 string sRequest = Constants.getDailyQueueAddRequest(); 
@@ -36,18 +31,7 @@ namespace DailyQueue
                 byte[] contentBytes = Encoding.ASCII.GetBytes(content);
 
                 // prepare the web page we will be asking for
-                HttpWebRequest request = (HttpWebRequest)
-                    WebRequest.Create(sRequest);
-                request.Method = "POST";
-                CookieContainer cookieJar = new CookieContainer();
-                cookieJar.Add(config.Cookies);
-                request.CookieContainer = cookieJar;
-                request.ContentLength = contentBytes.Length;
-                request.ContentType = "application/json";
-                Stream requestStream = request.GetRequestStream();
-                requestStream.Write(contentBytes, 0, contentBytes.Length);
-                requestStream.Close();
-
+                HttpWebRequest request = createHttpRequest(sRequest, "POST", "application/json", contentBytes, cookieJar, config); 
 
                 // execute the request
                 HttpWebResponse response;
@@ -62,30 +46,7 @@ namespace DailyQueue
                     return SendResult.FAILURE;
                 }
 
-                // we will read data via the response stream
-                Stream resStream = response.GetResponseStream();
-
-                string tempString = null;
-                int count = 0;
-
-                do
-                {
-                    // fill the buffer with data
-                    count = resStream.Read(buf, 0, buf.Length);
-
-                    // make sure we read some data
-                    if (count != 0)
-                    {
-                        // translate from bytes to ASCII text
-                        tempString = Encoding.ASCII.GetString(buf, 0, count);
-
-                        // continue building the string
-                        sb.Append(tempString);
-                    }
-                }
-                while (count > 0); // any more data to read?
-
-                string json = sb.ToString();
+                string json = parseHttpResponse(response);
                 JObject o = JObject.Parse(json);
                 JObject oLink = (JObject)o["link"];
                 if (oLink != null)
@@ -101,7 +62,7 @@ namespace DailyQueue
                 }
                 else
                 {
-                    if (authenticate(config) == SendResult.FAILURE)
+                    if (authenticate(ref config, cookieJar) == SendResult.FAILURE)
                     {
                         trying = false;
                     }
@@ -112,31 +73,16 @@ namespace DailyQueue
             
         }
 
-        private SendResult authenticate(DailyQueueConfiguration config)
+        private SendResult authenticate(ref DailyQueueConfiguration config, CookieContainer cookieJar)
         {
 
-            StringBuilder sb = new StringBuilder();
-
-            // used on each read operation
-            byte[] buf = new byte[8192];
-
-            //the request
-            //string sRequest = Constants.getDailyQueueAddRequest(user, link); 
+            //the request 
             string sRequest = Constants.getDailyQueueSignInRequest();
             string content = "{remote: true, commit: 'Sign in', utf8: '✓', user: {remember_me: 1, password: '" + config.Password + "', email: '" + config.User + "'}}";
             byte[] contentBytes = Encoding.ASCII.GetBytes(content);
 
             // prepare the web page we will be asking for
-            HttpWebRequest request = (HttpWebRequest)
-                WebRequest.Create(sRequest);
-            request.Method = "POST";
-            CookieContainer cookieJar = new CookieContainer();
-            request.CookieContainer = cookieJar;
-            request.ContentLength = contentBytes.Length;
-            request.ContentType = "application/json";
-            Stream requestStream = request.GetRequestStream();
-            requestStream.Write(contentBytes, 0, contentBytes.Length);
-            requestStream.Close();
+            HttpWebRequest request = createHttpRequest(sRequest, "POST", "application/json", contentBytes, cookieJar, config); 
 
             // execute the request
             HttpWebResponse response;
@@ -150,6 +96,47 @@ namespace DailyQueue
                 string message = e.Message;
                 return SendResult.FAILURE;
             }
+
+            string json = parseHttpResponse(response);
+            JObject o = JObject.Parse(json);
+            JValue oSuccess = (JValue)o["success"];
+            if ((bool)oSuccess.Value)
+            {
+                return SendResult.SUCCESS;
+            }
+            else
+            {
+                return SendResult.FAILURE;
+            }
+
+        }
+
+        private HttpWebRequest createHttpRequest(string sRequest, string method, string contentType, byte[] contentBytes, CookieContainer cookieJar, DailyQueueConfiguration config)
+        {
+            HttpWebRequest request = (HttpWebRequest)
+                WebRequest.Create(sRequest);
+            request.Method = method;
+            cookieJar.Add(config.Cookies);
+            request.CookieContainer = cookieJar;
+            request.ContentLength = contentBytes.Length;
+            request.ContentType = contentType;
+            if (contentBytes != null)
+            {
+                Stream requestStream = request.GetRequestStream();
+                requestStream.Write(contentBytes, 0, contentBytes.Length);
+                requestStream.Close();
+            }
+            return request;
+        }
+
+        private string parseHttpResponse(HttpWebResponse response)
+        {
+
+            // used to build entire input
+            StringBuilder sb = new StringBuilder();
+
+            // used on each read operation
+            byte[] buf = new byte[8192];
 
             // we will read data via the response stream
             Stream resStream = response.GetResponseStream();
@@ -174,18 +161,7 @@ namespace DailyQueue
             }
             while (count > 0); // any more data to read?
 
-            string json = sb.ToString();
-            JObject o = JObject.Parse(json);
-            JValue oSuccess = (JValue)o["success"];
-            if ((bool)oSuccess.Value)
-            {
-                return SendResult.SUCCESS;
-            }
-            else
-            {
-                return SendResult.FAILURE;
-            }
-
+            return sb.ToString();
         }
     }
 }
